1e8c5c6314d32b061f85cc6e7c32620d02575f6b
Frank Knoll <Knoll_Frank@web.de>
2019-12-16 15:59:53 +0100

    highlighting selected node

--- js/network.js
+++ js/network.js
@@ -16,17 +16,17 @@ function createNetworkNodesFromLeftAndRightNodeChild(node, attributeNames, depth
 
     const createOneLevelEdges = (fromNode, toNodes, label) =>
         toNodes
-        .filter(toNode => toNode.level == fromNode.level + 1)
-        .map(toNode => ({
-            from: fromNode.id,
-            to: toNode.id,
-            label: label
-        }));
+            .filter(toNode => toNode.level == fromNode.level + 1)
+            .map(toNode => ({
+                from: fromNode.id,
+                to: toNode.id,
+                label: label
+            }));
 
     let newEdges =
         createOneLevelEdges(newNode, leftNetwork.nodes, "true")
-        .concat(
-            createOneLevelEdges(newNode, rightNetwork.nodes, "false"));
+            .concat(
+                createOneLevelEdges(newNode, rightNetwork.nodes, "false"));
 
     return {
         nodes: [newNode].concat(leftNetwork.nodes, rightNetwork.nodes),
@@ -69,12 +69,36 @@ function displayNetwork(container, data) {
             hierarchical: {
                 direction: "UD"
             }
+        },
+        interaction: {
+            hover: true,
+            navigationButtons: true
         }
     };
-    network = new vis.Network(container, data, options);
+
+    const nodes = new vis.DataSet(data.nodes);
+
+    network = new vis.Network(container, { nodes: nodes, edges: data.edges }, options);
 
     // add event listeners
-    network.on('select', function(params) {
+    network.on('select', function (params) {
         document.getElementById('selection').innerHTML = 'Selection: ' + params.nodes;
+    });
+
+    // see https://stackoverflow.com/questions/38768598/vis-js-setoptions-to-change-color-on-network-node and http://jsfiddle.net/9knw26nc/1/
+    network.on("select", function (params) {
+        const nodeID = params['nodes']['0'];
+        if (nodeID) {
+            const clickedNode = nodes.get(nodeID);
+            clickedNode.color = {
+                border: '#000000',
+                background: '#000000',
+                highlight: {
+                    border: '#2B7CE9',
+                    background: '#D2E5FF'
+                }
+            };
+            nodes.update(clickedNode);
+        }
     });
 }
