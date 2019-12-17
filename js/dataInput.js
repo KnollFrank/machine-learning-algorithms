9b368453631368a9575e0515929f994bbe0b9f16
Frank Knoll <Knoll_Frank@web.de>
2019-12-17 20:50:26 +0100

    marking network edges

--- js/dataInput.js
+++ js/dataInput.js
@@ -11,6 +11,7 @@ function displayDataInput(dataInputForm, attributeNames, tree, network) {
             const prediction = predict(tree, getInputNumbersById(attributeNames));
             const nodeIdsOfPrediction = prediction.nodes.map(node => node.id);
             highlightTreeNodes(network.nodes, nodeIdsOfPrediction);
+            highlightTreeEdges(network.edges, nodeIdsOfPrediction);
             document.querySelector('#predicted').innerHTML = `${prediction.value}, Nodes: ${nodeIdsOfPrediction.join(', ')}`;
             return false;
         });
@@ -19,6 +20,28 @@ function displayDataInput(dataInputForm, attributeNames, tree, network) {
 function highlightTreeNodes(networkNodes, nodeIds2Highlight) {
     const networkNodes2Highlight = networkNodes.get({ filter: networkNode => nodeIds2Highlight.includes(networkNode.id) });
     highlightNodes(networkNodes, networkNodes2Highlight);
+}
+
+// TODO: refactor
+function highlightTreeEdges(networkEdges, nodeIds2Highlight) {
+    const networkEdges2Highlight = [];
+    for (let i = 0; i < nodeIds2Highlight.length - 1; i++) {
+        const networkEdge = getNetworkEdge(networkEdges, nodeIds2Highlight[i], nodeIds2Highlight[i + 1]);
+        networkEdges2Highlight.push(networkEdge);
+    }
+    highlightEdges(networkEdges, networkEdges2Highlight);
+}
+
+function getNetworkEdge(networkEdges, id1, id2) {
+    return networkEdges.get({
+        filter: networkEdge => isNetworkEdgeBetweenNodeIds(networkEdge, id1, id2)
+    })[0];
+}
+
+function isNetworkEdgeBetweenNodeIds(networkEdge, nodeId1, nodeId2) {
+    const isNetworkEdgeFromTo = (fromId, toId) => networkEdge.from == fromId && networkEdge.to == toId;
+
+    return isNetworkEdgeFromTo(nodeId1, nodeId2) || isNetworkEdgeFromTo(nodeId2, nodeId1);
 }
 
 function appendInputElements(parent, attributeNames) {
