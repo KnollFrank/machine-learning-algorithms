7a8b89575bf0b7a7e649642fcd3a281f1ee4f53b
Frank Knoll <Knoll_Frank@web.de>
2019-12-22 23:49:29 +0100

    refactoring

--- public/js/decisionTreeWorker.js
+++ public/js/decisionTreeWorker.js
@@ -4,39 +4,49 @@ importScripts('idGenerator.js');
 importScripts('decisionTree.js');
 
 onmessage = function(e) {
-    const { dataset, max_depth, min_size } = e.data;
-    let rootNode;
-    let lastPostMessageTime = new Date().getTime();
-    const waitTime = 100;
-    const tree = new DecisionTreeBuilder(
-        max_depth,
-        min_size, {
-            onNodeAdded: node => {
-                if (!rootNode) {
-                    rootNode = node;
-                }
-                const actualPostMessageTime = new Date().getTime();
-                if (actualPostMessageTime - lastPostMessageTime >= waitTime) {
-                    lastPostMessageTime = actualPostMessageTime;
-                    postMessage({
-                        type: 'info',
-                        value: rootNode
-                    });
-                }
-            },
-            onEdgeAdded: (fromNode, toNode) => {
-                const actualPostMessageTime = new Date().getTime();
-                if (actualPostMessageTime - lastPostMessageTime >= waitTime) {
-                    lastPostMessageTime = actualPostMessageTime;
-                    postMessage({
-                        type: 'info',
-                        value: rootNode
-                    });
-                }
-            }
-        }).build_tree(dataset);
     postMessage({
         type: 'result',
-        value: tree
+        value: build_tree(e.data)
     });
+}
+
+function build_tree({ dataset, max_depth, min_size }) {
+    return new DecisionTreeBuilder(
+            max_depth,
+            min_size,
+            createTreeListener())
+        .build_tree(dataset);
+}
+
+function createTreeListener() {
+    const timedExecutor = new TimedExecutor(100);
+    let rootNode;
+    return {
+        onNodeAdded: node => {
+            if (!rootNode) {
+                rootNode = node;
+            }
+            timedExecutor.execute(() => postMessage({ type: 'info', value: rootNode }));
+        },
+        onEdgeAdded: (fromNode, toNode) => {
+            timedExecutor.execute(() => postMessage({ type: 'info', value: rootNode }));
+        }
+    }
+}
+
+class TimedExecutor {
+    constructor(waitTimeMillis) {
+        this.waitTimeMillis = waitTimeMillis;
+        this.lastExecutionTime = new Date().getTime();
+        this.firstExecution = true;
+    }
+
+    execute(callback) {
+        const actualExecutionTime = new Date().getTime();
+        if (actualExecutionTime - this.lastExecutionTime >= this.waitTimeMillis || this.firstExecution) {
+            this.firstExecution = false;
+            this.lastExecutionTime = actualExecutionTime;
+            callback();
+        }
+    }
 }
