ce3a7bd3fd4fdc4371c656a9bf7c88646e5db762
Frank Knoll <Knoll_Frank@web.de>
2019-12-29 00:53:22 +0100

    refactoring

--- public/js/main.js
+++ public/js/main.js
@@ -61,28 +61,62 @@ function isDigitDataset(datasetDescription) {
     return datasetDescription.fileName.toLowerCase().startsWith('mnist');
 }
 
+class DisplayDigitDatasetTemplate {
+
+    constructor() {}
+
+    displayDigitDataset(digitDataset, digitsContainerId) {
+        const digitsContainer = document.querySelector('#' + digitsContainerId);
+        for (let i = 0; i < digitDataset.length; i++) {
+            const digit = new Digit();
+            digit.setFigcaption(...this._getFigcaption(digitDataset[i]));
+            digit.setImage(digitDataset[i]);
+            digitsContainer.appendChild(digit.digitElement);
+        }
+    }
+
+    _getFigcaption(row) {
+        throw new Error('You have to build your own figcaption');
+    }
+}
+
+class DisplayDigitTrainDataset extends DisplayDigitDatasetTemplate {
+
+    constructor() {
+        super();
+    }
+
+    _getFigcaption(row) {
+        return [getClassValFromRow(row)];
+    }
+}
+
 function displayDigitTrainDataset(datasetDescription) {
-    const digitsContainer = document.querySelector('#container-digits-training');
-    const rows = datasetDescription.splittedDataset.train;
-    for (let i = 0; i < rows.length; i++) {
-        const digit = new Digit();
-        digit.setFigcaption(getClassValFromRow(rows[i]));
-        digit.setImage(rows[i]);
-        digitsContainer.appendChild(digit.digitElement);
+    new DisplayDigitTrainDataset()
+        .displayDigitDataset(
+            datasetDescription.splittedDataset.train,
+            'container-digits-training');
+}
+
+class DisplayDigitTestDataset extends DisplayDigitDatasetTemplate {
+
+    constructor(tree) {
+        super();
+        this.tree = tree;
+    }
+
+    _getFigcaption(row) {
+        const actualDigit = getClassValFromRow(row);
+        const predictedDigit = predict(this.tree, row).value;
+        return [`actual: ${actualDigit}, predicted: ${predictedDigit}`, actualDigit != predictedDigit ? 'wrongPrediction' : undefined];
     }
 }
 
 function displayDigitTestDataset(datasetDescription, tree) {
-    const digitsContainer = document.querySelector('#container-digits-test');
-    const rows = datasetDescription.splittedDataset.test;
-    for (let i = 0; i < rows.length; i++) {
-        const actualDigit = getClassValFromRow(rows[i]);;
-        const predictedDigit = predict(tree, rows[i]).value;
-        const digit = new Digit();
-        digit.setFigcaption(`actual: ${actualDigit}, predicted: ${predictedDigit}`, actualDigit != predictedDigit ? 'wrongPrediction' : undefined);
-        digit.setImage(rows[i]);
-        digitsContainer.appendChild(digit.digitElement);
-    }
+    new DisplayDigitTestDataset(tree)
+        .displayDigitDataset(
+            datasetDescription.splittedDataset.test,
+            'container-digits-test');
 }
 
 let submitEventListener;
