564874522861539511553bd328a50954eeec16d0
Frank Knoll <Knoll_Frank@web.de>
2019-12-29 10:41:20 +0100

    continuing

--- public/js/main.js
+++ public/js/main.js
@@ -188,9 +188,22 @@ function drawTool() {
     let mousey = 0;
     let mousedown = false;
 
+    // taken from https://stackoverflow.com/questions/17130395/real-mouse-position-in-canvas
+    function getMousePos(canvas, evt) {
+        const rect = canvas.getBoundingClientRect(), // abs. size of element
+            scaleX = canvas.width / rect.width, // relationship bitmap vs. element for X
+            scaleY = canvas.height / rect.height; // relationship bitmap vs. element for Y
+
+        return {
+            x: (evt.clientX - rect.left) * scaleX, // scale mouse coordinates after they have
+            y: (evt.clientY - rect.top) * scaleY // been adjusted to be relative to element
+        }
+    }
+
     $(canvas).on('mousedown', function(e) {
-        last_mousex = mousex = parseInt(e.clientX - canvasx);
-        last_mousey = mousey = parseInt(e.clientY - canvasy);
+        const pos = getMousePos(canvas, e);
+        last_mousex = mousex = pos.x;
+        last_mousey = mousey = pos.y;
         mousedown = true;
     });
 
@@ -199,8 +212,9 @@ function drawTool() {
     });
 
     $(canvas).on('mousemove', function(e) {
-        mousex = parseInt(e.clientX - canvasx);
-        mousey = parseInt(e.clientY - canvasy);
+        const pos = getMousePos(canvas, e);
+        mousex = pos.x;
+        mousey = pos.y;
         if (mousedown) {
             ctx.beginPath();
             ctx.globalCompositeOperation = 'source-over';
