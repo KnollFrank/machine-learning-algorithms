3a3e4142282c39eed1c5148dfe018b62d4b99bac
Frank Knoll <Knoll_Frank@web.de>
2020-01-01 17:32:58 +0100

    making test "k nearest neighbors" pass

--- public/js/KNN.js
+++ public/js/KNN.js
@@ -6,10 +6,16 @@ class KNN {
     }
 
     fit(X, y) {
-
+        this.X = X;
+        this.y = y;
     }
 
     predict(x) {
-        return 0;
+        const distances2x = [];
+        for (let i = 0; i < this.X.length; i++) {
+            distances2x.push({ index: i, distance: Math.sqrt((this.X[i] - x) ** 2) });
+        }
+        distances2x.sort((a, b) => a.distance - b.distance);
+        return this.y[distances2x[0].index];
     }
 }
