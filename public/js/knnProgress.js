cfeb7396af9649b6c3dd6d1ee7a23e620e25b5ad
Frank Knoll <Knoll_Frank@web.de>
2020-01-11 18:54:03 +0100

    continuing

--- public/js/knnProgress.js
+++ public/js/knnProgress.js
@@ -2,56 +2,48 @@
 
 function createKnnProgressElements(progressId, numWorkers) {
     let progressTable = document.querySelector(`#${progressId} div.table`);
-    clearTable(progressTable);
-    appendRows(progressTable, numWorkers);
+    clearKnnTable(progressTable);
+    appendKnnRows(progressTable, numWorkers);
 }
 
-function clearTable(table) {
+function clearKnnTable(table) {
     table.querySelectorAll('div.table-row').forEach(row => row.remove());
 }
 
-function appendRows(parent, numWorkers) {
+function appendKnnRows(parent, numWorkers) {
     for (let workerIndex = 0; workerIndex < numWorkers; workerIndex++) {
-        parent.appendChild(createRow(workerIndex));
+        parent.appendChild(createKnnRow(workerIndex));
     }
 }
 
-function createRow(workerIndex) {
-    const div = getHtml('progressTemplate.html');
-    div.setAttribute('id', createRowId(workerIndex));
+function createKnnRow(workerIndex) {
+    const div = getHtml('knnProgressTemplate.html');
+    div.setAttribute('id', createKnnRowId(workerIndex));
     return div;
 }
 
 function setKnnProgress_workerId(workerIndex, text) {
-    getTableCellOfTableRow(workerIndex, 'div.workerId').innerHTML = text;
-}
-
-function setProgress_startAttribute(workerIndex, text) {
-    getTableCellOfTableRow(workerIndex, 'div.startAttribute').innerHTML = text;
-}
-
-function setProgress_actualAttribute(workerIndex, text) {
-    getTableCellOfTableRow(workerIndex, 'div.actualAttribute').innerHTML = text;
+    getKnnTableCellOfTableRow(workerIndex, 'div.workerId').innerHTML = text;
 }
 
 function setKnnProgress_endAttribute(workerIndex, endIndexZeroBasedExclusive) {
-    getTableCellOfTableRow(workerIndex, 'div.endAttribute').innerHTML = endIndexZeroBasedExclusive;
+    getKnnTableCellOfTableRow(workerIndex, 'div.endAttribute').innerHTML = endIndexZeroBasedExclusive;
 }
 
 function setKnnProgress_progress({ workerIndex, value, max }) {
-    const progress = getTableCellOfTableRow(workerIndex, 'div.progress progress');
+    const progress = getKnnTableCellOfTableRow(workerIndex, 'div.progress progress');
     progress.value = value;
     progress.max = max;
 }
 
-function getTableCellOfTableRow(workerIndex, subElementSelector) {
-    return getTableRow(workerIndex).querySelector(subElementSelector);
+function getKnnTableCellOfTableRow(workerIndex, subElementSelector) {
+    return getKnnTableRow(workerIndex).querySelector(subElementSelector);
 }
 
-function getTableRow(workerIndex) {
-    return document.querySelector('#' + createRowId(workerIndex));
+function getKnnTableRow(workerIndex) {
+    return document.querySelector('#' + createKnnRowId(workerIndex));
 }
 
-function createRowId(workerIndex) {
-    return 'worker-' + workerIndex;
+function createKnnRowId(workerIndex) {
+    return 'knnWorker-' + workerIndex;
 }
