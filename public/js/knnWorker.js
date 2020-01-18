fcc6319ffee42f9416fbbebfb0ad966a6371a4c3
Frank Knoll <Knoll_Frank@web.de>
2020-01-18 15:03:28 +0100

    refactoring

--- public/js/knnWorker.js
+++ public/js/knnWorker.js
@@ -2,31 +2,11 @@
 
 importScripts('jsHelper.js');
 importScripts('KNN.js');
+importScripts('knnWorkerSync.js');
 
-let knn;
+const knnWorkerSync = new KnnWorkerSync();
 
 onmessage = e => {
-    const { type, params } = e.data;
-    switch (type) {
-        case 'fit':
-            {
-                const { X, y, k } = params;
-                knn = new KNN(k);
-                knn.fit(X, y);
-                break;
-            }
-        case 'getKNearestNeighbors':
-            {
-                const X = params.X
-                const kNearestNeighborss = X.map((x, index) => {
-                    postMessage({
-                        type: 'progress',
-                        value: { actualIndexZeroBased: index, endIndexZeroBasedExclusive: X.length }
-                    });
-                    return knn.getKNearestNeighbors(x);
-                });
-                postMessage({ type: 'result', value: kNearestNeighborss });
-                break;
-            }
-    }
+    knnWorkerSync.onmessage = e => postMessage(e.data);
+    knnWorkerSync.postMessage(e.data);
 }
