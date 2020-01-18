d0dc319061dd7606d9dd9abaaacf2425c02b2fab
Frank Knoll <Knoll_Frank@web.de>
2020-01-18 17:00:25 +0100

    refactoring

--- public/js/decisionTree/decisionTreeBuilder.js
+++ public/js/decisionTree/decisionTreeBuilder.js
@@ -2,28 +2,6 @@
 
 // adapted from https://machinelearningmastery.com/implement-decision-tree-algorithm-scratch-python/
 
-function splitItemsIntoChunks({
-    numItems,
-    maxNumChunks
-}) {
-    if (numItems == 0) {
-        return [];
-    }
-    const chunks = [];
-    const chunkSize = numItems < maxNumChunks ? 1 : Math.floor(numItems / maxNumChunks);
-    for (let chunkIndex = 0; chunkIndex < Math.min(maxNumChunks, numItems) - 1; chunkIndex++) {
-        chunks.push({
-            oneBasedStartIndexOfChunk: chunkIndex * chunkSize + 1,
-            oneBasedEndIndexInclusiveOfChunk: (chunkIndex + 1) * chunkSize
-        });
-    }
-    chunks.push({
-        oneBasedStartIndexOfChunk: (Math.min(maxNumChunks, numItems) - 1) * chunkSize + 1,
-        oneBasedEndIndexInclusiveOfChunk: numItems
-    });
-    return chunks;
-}
-
 const dummyTreeListener = {
     onNodeAdded: node => {},
     onEdgeAdded: (fromNode, toNode) => {},
