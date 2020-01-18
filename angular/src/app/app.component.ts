74937da15d27294d6fe241f8ea1d901810458e75
Frank Knoll <Knoll_Frank@web.de>
2020-01-18 20:36:28 +0100

    adding CacheService

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CacheService } from './cache.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular';
+
+  constructor(private cache: CacheService) {
+
+  }
+
+  ngOnInit(): void {
+    console.log(this.cache.get('someKey', () => Math.floor(Math.random()*100)));
+    console.log(this.cache.get('someKey', () => Math.floor(Math.random()*100)));
+  }
 }
---
+++ angular/src/app/cache.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+
+declare var zip: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CacheService {
+  cache: {};
+
+  constructor() {
+    this.cache = {};
+  }
+
+  get(key, computeValue) {
+    // FK-TODO: Vergleich geht glaube ich über eine Stringkonvertierung des keys, was nicht gut ist. Anders prüfen mit compareFlatArrays().
+    if (!this.containsKey(key)) {
+      this._cacheValueForKey({ key, value: computeValue() });
+    }
+    return this._getValueForKey(key);
+  }
+
+  containsKey(key) {
+    return this.cache.hasOwnProperty(key);
+  }
+
+  cacheValuesForKeys({ keys, values }) {
+    zip(keys, values).forEach(([key, value]) => this._cacheValueForKey({ key, value }));
+  }
+
+  _cacheValueForKey({ key, value }) {
+    this.cache[key] = value;
+  }
+
+  getValuesForKeys({ keys }) {
+    return keys.map(key => this._getValueForKey(key));
+  }
+
+  _getValueForKey(key) {
+    return this.cache[key];
+  }
+}
