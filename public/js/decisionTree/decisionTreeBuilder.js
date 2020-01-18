afba7c01e400acc22ccdd74db319a8ee1af26f3d
Frank Knoll <Knoll_Frank@web.de>
2020-01-18 16:43:01 +0100

    refactoring

--- public/js/decisionTree/decisionTreeBuilder.js
+++ public/js/decisionTree/decisionTreeBuilder.js
@@ -244,56 +244,6 @@ function print_tree(node, attributeNames) {
     _print_tree(node, 0);
 }
 
-class EnhancedNodeContentFactory {
-
-    getInnerNodeContent(node, attributeNames) {
-        return `${getTestNodeText(node, attributeNames)}
-${getGiniNodeText(node)}
-${getAnzahlNodeText(node)}`;
-    }
-
-    getTerminalNodeContent(node) {
-        return `${getVorhersageNodeText(node)}
-${getGiniNodeText(node)}
-${getAnzahlNodeText(node)}`;
-    }
-}
-
-class SimpleNodeContentFactory {
-
-    getInnerNodeContent(node, attributeNames) {
-        return getTestNodeConditionText(node, attributeNames);
-    }
-
-    getTerminalNodeContent(node) {
-        return node.value;
-    }
-}
-
-function getTestNodeText(node, attributeNames) {
-    return `Test = <b>"${getTestNodeConditionText(node, attributeNames)}"</b>`;
-}
-
-function getTestNodeConditionText(node, attributeNames) {
-    return `${attributeNames[node.index]} ${isNumber(node.value) ? '<' : '='} ${node.value}`;
-}
-
-function getGiniNodeText(node) {
-    return `gini = ${toFixed4Digits(node.score)}`;
-}
-
-function getAnzahlNodeText(node) {
-    return `Anzahl Datensätze = ${node.samples}`;
-}
-
-function getVorhersageNodeText(node) {
-    return `Vorhersage = <b>${node.value}</b>`;
-}
-
-function toFixed4Digits(x) {
-    return Number.parseFloat(x).toFixed(4);
-}
-
 // Make a prediction with a decision tree
 function predict(node, row) {
     if (!node) {
