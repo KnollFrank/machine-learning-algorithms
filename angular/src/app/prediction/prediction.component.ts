aba8b22a766aba378da245e71f51955d7bf81a8e
Frank Knoll <Knoll_Frank@web.de>
2020-01-20 11:45:32 +0100

    starting drawingTool within prediction component

--- angular/src/app/prediction/prediction.component.ts
+++ angular/src/app/prediction/prediction.component.ts
@@ -1,17 +1,38 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-prediction',
   templateUrl: './prediction.component.html',
   styleUrls: ['./prediction.component.css']
 })
-export class PredictionComponent implements OnInit {
+export class PredictionComponent implements OnInit, AfterViewInit {
 
   @Input() knnClassifier;
+
+  @ViewChild('digitCanvasBig', { static: false }) public canvasBig: ElementRef<HTMLCanvasElement>;
+
+  private ctxBig: CanvasRenderingContext2D;
 
   constructor() { }
 
   ngOnInit() {
     console.log('knnClassifier:', this.knnClassifier);
+  }
+
+  ngAfterViewInit(): void {
+    this.ctxBig = this.canvasBig.nativeElement.getContext('2d');
+    // this.drawImageIntoCanvas();
+  }
+
+  mousedown(e) {
+    console.log(e.type);
+  }
+
+  mousemove(e) {
+    console.log(e.type);
+  }
+
+  mouseup(e) {
+    console.log(e.type);
   }
 }
