18ed93016d64154b0b342f51d74f3ae8789c5216
Frank Knoll <Knoll_Frank@web.de>
2020-01-26 21:46:13 +0100

    refactoring

--- angular/src/app/knn-progress/knn-progress.component.ts
+++ angular/src/app/knn-progress/knn-progress.component.ts
@@ -7,19 +7,19 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class KnnProgressComponent implements OnInit {
 
-  // FK-TODO: rename numWorkers to numProgessElements
-  @Input() numWorkers;
+  @Input() numProgessElements;
   progress: { actualProgress: number; maxProgress: number; }[];
 
   constructor() { }
 
   ngOnInit() {
     this.progress = [];
-    for (let i = 0; i < this.numWorkers; i++) {
+    for (let i = 0; i < this.numProgessElements; i++) {
       this.progress.push({ actualProgress: 0, maxProgress: 0 });
     }
   }
 
+  // FK-TODO: rename workerIndexZeroBased to progressElementIndexZeroBased
   setProgress({ workerIndexZeroBased, actualIndexZeroBased, endIndexZeroBasedExclusive }) {
     this.progress[workerIndexZeroBased].actualProgress = actualIndexZeroBased + 1;
     this.progress[workerIndexZeroBased].maxProgress = endIndexZeroBasedExclusive;
