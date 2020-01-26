fd318b3ba6631d138320e57c43e78601d31e6610
Frank Knoll <Knoll_Frank@web.de>
2020-01-26 17:17:37 +0100

    refactoring

--- angular/src/app/knn/knn.worker.ts
+++ angular/src/app/knn/knn.worker.ts
@@ -1,39 +1,5 @@
 /// <reference lib="webworker" />
-import {KNN} from './KNN';
-
-///////////////////////////
-'use strict';
-
-class KnnWorker {
-
-    knn;
-    postMessage;
-
-    onmessage({ type, params }) {
-        switch (type) {
-            case 'fit':
-                {
-                    const { X, y, k } = params;
-                    this.knn = new KNN(k);
-                    this.knn.fit(X, y);
-                    break;
-                }
-            case 'getKNearestNeighbors':
-                {
-                    const X = params.X
-                    const kNearestNeighborss = X.map((x, index) => {
-                        this.postMessage({
-                            type: 'progress',
-                            value: { actualIndexZeroBased: index, endIndexZeroBasedExclusive: X.length }
-                        });
-                        return this.knn.getKNearestNeighbors(x);
-                    });
-                    this.postMessage({ type: 'result', value: kNearestNeighborss });
-                    break;
-                }
-        }
-    }
-}
+import { KnnWorker } from './knnWorker';
 
 const knnWorker = new KnnWorker();
 
