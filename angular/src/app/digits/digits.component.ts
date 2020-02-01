90d1bbe1c44c909b8e04a8abf1a810c03776b8ed
Frank Knoll <Knoll_Frank@web.de>
2020-02-01 13:01:23 +0100

    paginating digits

--- angular/src/app/digits/digits.component.ts
+++ angular/src/app/digits/digits.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-
-declare var getClassValFromRow: any;
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-digits',
@@ -11,8 +10,36 @@ export class DigitsComponent implements OnInit {
 
   @Input() digitDataset;
 
+  pagedDigitDataset = [];
+  pageSizeOptions: number[] = [80, 160, 240];
+  pageEvent: PageEvent;
+
+  get pageSize() {
+    return this.pageEvent ? this.pageEvent.pageSize : this.pageSizeOptions[0];
+  }
+
+  get pageIndex() {
+    return this.pageEvent ? this.pageEvent.pageIndex : 0;
+  }
+
+  get length() {
+    return this.digitDataset ? this.digitDataset.length : 0;
+  }
+
   constructor() { }
 
   ngOnInit() {
+    this.updatePagedDigitDataset();
+  }
+
+  handlePageEvent(pageEvent) {
+    this.pageEvent = pageEvent;
+    this.updatePagedDigitDataset();
+  }
+
+  private updatePagedDigitDataset() {
+    const start = this.pageIndex * this.pageSize;
+    const end = (this.pageIndex + 1) * this.pageSize;
+    this.pagedDigitDataset = this.digitDataset ? this.digitDataset.slice(start, end) : [];
   }
 }
