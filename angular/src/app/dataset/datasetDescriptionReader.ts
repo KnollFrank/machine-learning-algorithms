0130c05e7ec6bb5bd6c5723c335e500339076337
Frank Knoll <Knoll_Frank@web.de>
2020-02-05 12:27:38 +0100

    exchanging test dataset

--- angular/src/app/dataset/datasetDescriptionReader.ts
+++ angular/src/app/dataset/datasetDescriptionReader.ts
@@ -7,7 +7,7 @@ export class DatasetDescriptionReader {
   public readDatasetDescription(receiveDatasetDescription) {
     this.readCSVFiles(
       './assets/mnist_train_5000.csv',
-      './assets/mnist_test_1000.csv',
+      './assets/mnist_test_10000.csv',
       (trainDataset, testDataset) => {
         const datasetDescription = this.getDatasetDescription(trainDataset, testDataset);
         receiveDatasetDescription(datasetDescription);
